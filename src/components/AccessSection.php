393bb4c5f3fee60c9548d9d3448a8a6a590a0861
ravenloue <42080046+ravenloue@users.noreply.github.com>
2023-06-24 13:37:29 -0500

    Adding components
    I broke the persona card outside of the logged in section. Hoping to stack the other cards below it in the logged in section. Testing this phase soon.

--- src/components/AccessSection.php
+++ src/components/AccessSection.php
@@ -5,15 +5,15 @@
     $aside="
         <aside id='AccessSection' class='container'>
             <h4>Simple Bank</h3>
-            <img src='./src/img/bank.png' style='width: 125px; height: 125px;'/>
+            <img src='./src/img/bank.png' style='width: 125px; height: 125px; margin-left: 25px;'/>
             <p>(555) 555 - 0123<br />
             support@simplebank.com</p>
             <br />
-            <button type='submit' class='login-btn' name='submit' id='acctBtn'>Accounts</button><br />
-            <button type='submit' class='login-btn' name='submit' id='transferBtn'>Transfer</button><br />
-            <button type='submit' class='login-btn' name='submit' id='depositBtn'>Deposit</button><br />
-            <button type='submit' class='login-btn' name='submit' id='moreBtn'>More ...</button><br />
-            <form action='./src/includes/logout.inc.php'><button type='submit' class='login-btn' name='submit' id='logOutBtn'>Log Out</button></form>
+            <button type='submit' class='login-btn' name='submit' id='acctBtn'><img src='./src/img/home.svg' height='30' alt='home icon' /> Home</button><br />
+            <button type='submit' class='login-btn' name='submit' id='transferBtn'><img src='./src/img/dollar.svg' height='30' alt='dollar icon' /> Account</button><br />
+            <button type='submit' class='login-btn' name='submit' id='depositBtn'><img src='./src/img/list-circle.svg' height='30' alt='list icon' /> Transactions</button><br />
+            <button type='submit' class='login-btn' name='submit' id='moreBtn'><img src='./src/img/square-more.svg' height='30' alt='more icon' /> More ...</button><br />
+            <form action='./src/includes/logout.inc.php'><button type='submit' class='login-btn' name='submit' id='logOutBtn'><img src='./src/img/logout.svg' height='30' alt='logout icon' /> Log Out</button></form>
             <br />
             <div id='calendar'>";
             
--- src/components/LoggedInSection.php
+++ src/components/LoggedInSection.php
@@ -1,14 +1,7 @@
 <?php
-    $section="
-    <div id='LoggedInSection' class='container'>
-        <div id='PersonaCard' class='border border-dark rounded-bottom flex-grid'>
-        <div class='border border-light border-4 rounded-circle'><img src='./src/img/DefaultProfileIcon.svg' width='100' alt='default profile icon'></div>
-            <div>
-            <h3>Welcome $name </h3><br />
-            <h5> Balance: </h6><p>$balance</p><br />
-            </div>
-        
-        </div>
-    </div>
-    ";
+    require "./src/components/PersonaCard.php";
+    
+    $section="<div id='LoggedInSection' class='container'>";
+    $section.=$article;   
+    $section.="</div>";
 ?>
