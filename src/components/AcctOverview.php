e222b7e0a7f9c667041f0ecbe202ad738bb188d4
ravenloue <42080046+ravenloue@users.noreply.github.com>
2023-06-24 18:07:44 -0500

    Transaction card progress
    Made some changes to the login.class to allow for more information to be passed to the session id. Updated loggedin.php to reflect this.
    Updated the dashboard to include transaction card.

--- src/components/AcctOverview.php
+++ src/components/AcctOverview.php
@@ -3,7 +3,7 @@
         <article id='AcctOverview' class='col-2'>
             <h4>My Account</h4><br />
             <h2> $balance </h2><br /><br/>
-            <h6> Simple Bank Test</h6>
+            <h6> Simple Bank $accType</h6>
         </article>
     ";
 ?>
--- src/views/dashboard.php
+++ src/views/dashboard.php
@@ -3,10 +3,13 @@
     require "./src/components/AcctOverview.php";
     require "./src/components/AcctCards.php";
     require "./src/components/News.php";
+    require "./src/components/AcctTransactions.php";
 
     $dashboard="<section id='Dashboard' class='container'>
                     <article class='row'>";
     $dashboard.=$overviewCard.$acctCards.$newsCard;
     $dashboard.="   </article>
-                 </section>";
+                 <article class='row'>";
+    $dashboard.=$transactions;
+    $dashboard.="</article></section>";
 ?>
